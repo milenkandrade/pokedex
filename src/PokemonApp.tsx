01b0d039382123894ed5783e1cb3ddfc101ae712
Marcia Andrade <marcia.andrade.llanos@gmail.com>
2025-01-28 23:37:14 -0400

    style:tailwind added

---
+++ src/PokemonApp.tsx
@@ -0,0 +1,7 @@
+
+
+export const PokemonApp = () => {
+  return (
+    <div className="text-4xl bg-amber-700">Welcome! Please keep in mind that this is a commercial website that lists wallets, exchanges and other bitcoin related info.</div>
+  )
+}
